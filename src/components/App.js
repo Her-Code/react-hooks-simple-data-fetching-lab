35efe8e36312f3ed08a06322afe5ac32d57b2a73
+Sharon <+wanjiku.kahira@gmail.com>
2024-08-13 14:05:02 +0300

    Use async function with UseEffect hook

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,42 @@
 // create your App component here
+import React,{ useEffect ,useState } from "react";
+
+function App() {
+    const[dogImage,setDogImage]=useState(null);
+    const [ loading,setLoading]=useState(true);
+
+
+useEffect(()=>{
+    async function fetchDogImage(){
+        try {
+            const response =await fetch ('https://dog.ceo/api/breeds/image/random');
+            const data = await response.json();
+            setDogImage(data.message);
+            setLoading(false);
+        } catch(error) {
+            console.error("Error fetching dog image:",error);
+            setLoading(false);
+        };
+    }
+  fetchDogImage();
+
+
+},[]);
+
+let content;
+
+if (loading) {
+    content=<p>loading...</p>;
+} else  {
+    content =<img src={dogImage} alt="A Random Dog"></img>;
+}
+
+return(
+    <div>{content}</div>
+);
+}
+
+export default App;
+
+
+
